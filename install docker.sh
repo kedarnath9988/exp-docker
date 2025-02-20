d18e7855b9324bcc92bd70982ed9e4f0d76c8815
kedarnath9988 <anubolukedarnath9988@gmail.com>
2025-02-20 11:00:36 +0530

    docker installletion script

---
+++ install docker.sh
@@ -0,0 +1,10 @@
+/bin/bash
+R="\e[31m"
+N="\e[0m"
+dnf -y install dnf-plugins-core
+dnf config-manager --add-repo https://download.docker.com/linux/rhel/docker-ce.repo
+dnf install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y 
+systemctl enable docker 
+systemctl start docker 
+usermod -aG docker ec2-user 
+echo -e "$R  logout and login $N" 
